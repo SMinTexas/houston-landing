2cf53f19656538e03f31e909031e60e7fdd8b4c2
Alina Belova <belovaalni@gmail.com>
2019-10-02 16:01:14 -0500

    crime data file added

---
+++ crimeJSON.js
@@ -0,0 +1,150 @@
+var crime_annual_data = [{
+    "city": "Katy",
+    "state": "TX",
+    "zip": "77494",
+    "murder":"0.0" ,
+    "rape": "0.71",
+    "robbery": "0.82",
+    "assault": "1.59",
+    "burglary": "3.23",
+    "theft": "31.29",
+    "motor_vehicle_theft": "1.91",
+    "violent_crime": "3.12",
+    "property_crime": "36.43"
+    
+},
+{
+    "city": "Sugar Land",
+    "state": "TX",
+    "zip": "77479",
+    "murder":"0.0" ,
+    "rape": "0.17",
+    "robbery": "0.41",
+    "assault": "0.23",
+    "burglary": "1,79",
+    "theft": "12.15",
+    "motor_vehicle_theft": "0.63",
+    "violent_crime": "0.8",
+    "property_crime": "14.57",
+},
+{
+    "city": "Houston",
+    "state": "TX",
+    "zip": "77065",
+    "murder":"0.12" ,
+    "rape": "0.60",
+    "robbery": "4.26",
+    "assault": "6.20",
+    "burglary": "7.46",
+    "theft": "29.95",
+    "motor_vehicle_theft": "5.05",
+    "violent_crime": "11.17",
+    "property_crime": "42.46"
+
+},
+{
+    "city": "Conroe",
+    "state": "TX",
+    "zip": "77304",
+    "murder":"0.05" ,
+    "rape": "0.78",
+    "robbery": "0.78",
+    "assault": "1.62",
+    "burglary": "3.35",
+    "theft": "19.15",
+    "motor_vehicle_theft": "2.11",
+    "violent_crime": "3.24",
+    "property_crime": "24.62"
+
+},
+{
+    "city": "Humble",
+    "state": "TX",
+    "zip": "77347",
+    "murder":"0.05" ,
+    "rape": "0.42",
+    "robbery": "0.98",
+    "assault": "2.49",
+    "burglary": "8.63",
+    "theft": "81.64",
+    "motor_vehicle_theft": "11.19",
+    "violent_crime": "9.63",
+    "property_crime": "101.46"
+},
+{
+    "city": "La Porte",
+    "state": "TX",
+    "zip": "77571",
+    "murder":"0.0" ,
+    "rape": "0.57",
+    "robbery": "0.45",
+    "assault": "1.89",
+    "burglary": "2.89",
+    "theft": "11.37",
+    "motor_vehicle_theft": "1.39",
+    "violent_crime": "2.91",
+    "property_crime": "14.93"
+},
+{
+
+    "city": "League City",
+    "state": "TX",
+    "murder":"0.03" ,
+    "rape": "0.32",
+    "robbery": "0.17",
+    "assault": "0.42",
+    "burglary": "2.09",
+    "theft": "13.50",
+    "motor_vehicle_theft": "0.89",
+    "violent_crime": "0.94",
+    "property_crime": "16.47"
+},
+{
+    "city": "Pearland",
+    "state": "TX",
+    "zip": "77581",
+    "murder":"unreported" ,
+    "rape": "unreported",
+    "robbery": "unreported",
+    "assault": "unreported",
+    "burglary": "unreported",
+    "theft": "unreported",
+    "motor_vehicle_theft": "unreported",
+    "violent_crime": "0.57",
+    "property_crime": "14.47"
+},
+{
+    "city": "Cypress",
+    "state": "TX",
+    "zip": "77092",
+    "murder":"0" ,
+    "rape": "13",
+    "robbery": "15",
+    "assault": "29",
+    "burglary": "59",
+    "theft": "572",
+    "motor_vehicle_theft": "35",
+    "violent_crime": "1.24",
+    "property_crime": "13.29"
+},
+{
+    "city": "Bellaire",
+    "state": "TX",
+    "zip": "77092",
+    "murder":"0.0" ,
+    "rape": "0.21",
+    "robbery": "1.06",
+    "assault": "0.11",
+    "burglary": "2.39",
+    "theft": "13.03",
+    "motor_vehicle_theft": "0.64",
+    "violent_crime": "1.38",
+    "property_crime": "16.07"
+}
+]
+
+var state_avg = [{
+    "violent_crime": "4.39",
+    "property_crime": "25.63"
+    
+}]
